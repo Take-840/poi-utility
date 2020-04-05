06e8f070763579fadb2b6edb1b883d154bceeac9
Takeshi <takeshi.yajima.wz@gmail.com>
2020-04-05 17:49:35 +0900

    first

---
+++ src/main/java/utility/poi/App.java
@@ -0,0 +1,13 @@
+package utility.poi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
